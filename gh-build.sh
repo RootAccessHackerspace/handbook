a33888838e69dc43badec34f64170d5b90fcc07c
dmpayton <derek.payton@gmail.com>
2021-09-25 17:36:30 -0700

    First stab at building in github actions.

---
+++ gh-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+##############
+# BUILD DOCS #
+##############
+
+# Python Sphinx, configured with source/conf.py
+# See https://www.sphinx-doc.org/
+make clean
+make html
+
+#######################
+# Update GitHub Pages #
+#######################
+
+# Configure the user info.
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+# Add the deploy remote
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+ghp-import build/html/ --push --no-jekyll --remote deploy
+
+# exit cleanly
+exit 0
